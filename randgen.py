509bc4e2c1766e4e68f8da92d7f6b9c385282222
= <=>
2018-04-20 20:46:14 +0000

    transport

--- randgen.py
+++ randgen.py
@@ -1,6 +1,6 @@
 import random
 
-N = 100
+N = 1000
 
 
 l = [random.randint(0, 1000) for r in xrange(N)]
@@ -8,4 +8,5 @@ l = [random.randint(0, 1000) for r in xrange(N)]
 with open('input.txt', 'w') as fp:
     fp.write(str(len(l)) + "\n")
     fp.write(str(" ".join(str(x) for x in l)));
+    fp.write("\n")
 
---
+++ script.sh
@@ -0,0 +1,1 @@
+mpirun -H Master,Master,Master,Master,Slave1,Slave1,Slave1,Slave1,Slave2,Slave2,Slave2,Slave3,Slave3 bubble input.txt output.txt
