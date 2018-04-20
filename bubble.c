0a99cfbd175c72aa432001731aa0361c87432aaf
Hans Pistor <hpistor@stetson.edu>
2018-04-20 17:19:46 -0400

    code cleanup

--- bubble.c
+++ bubble.c
@@ -4,25 +4,25 @@
 #include <mpi.h>
 
 
-void swap(int * v, int i, int j)
+void swap(int* arr, int i, int j)
 {
-  int t = v[i];
-  v[i] = v[j];
-  v[j] = t;
+  int t = arr[i];
+  arr[i] = arr[j];
+  arr[j] = t;
 }
 
 
-void bubblesort(int* v, int n)
+void bubblesort(int* arr, int n)
 {
   int i, j;
   for (i = n-2; i >= 0; i--)
     for (j = 0; j <= i; j++)
-      if (v[j] > v[j+1])
-        swap(v, j, j+1);
+      if (arr[j] > arr[j+1])
+        swap(arr, j, j+1);
 }
 
 
-int* merge(int* v1, int n1, int* v2, int n2)
+int* merge(int* arr1, int n1, int* arr2, int n2)
 {
   int* result = (int*)malloc((n1 + n2) * sizeof(int));
   int i = 0;
@@ -30,19 +30,19 @@ int* merge(int* v1, int n1, int* v2, int n2)
   int k;
   for (k = 0; k < n1 + n2; k++) {
     if (i >= n1) {
-      result[k] = v2[j];
+      result[k] = arr2[j];
       j++;
     }
     else if (j >= n2) {
-      result[k] = v1[i];
+      result[k] = arr1[i];
       i++;
     }
-    else if (v1[i] < v2[j]) {
-      result[k] = v1[i];
+    else if (arr1[i] < arr2[j]) {
+      result[k] = arr1[i];
       i++;
     }
     else {
-      result[k] = v2[j];
+      result[k] = arr[j];
       j++;
     }
   }
@@ -50,25 +50,20 @@ int* merge(int* v1, int n1, int* v2, int n2)
 }
 
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
   int n;
-  int * data = NULL;
+  int* data = NULL;
   int c, s;
-  int * chunk;
+  int* chunk;
   int o;
-  int * other;
+  int* other;
   int step;
   int p, id;
   MPI_Status status;
   double elapsed_time;
-  FILE * file = NULL;
+  FILE* file = NULL;
   int i;
-
-  if (argc!=3) {
-    fprintf(stderr, "Usage: mpirun -H <hosts> %s <in_file> <out_file>\n", argv[0]);
-    exit(1);
-  }
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
@@ -77,7 +72,10 @@ int main(int argc, char ** argv)
   if (id == 0) {
     file = fopen(argv[1], "r");
     fscanf(file, "%d", &n);
-    c = n/p; if (n%p) c++;
+    c = n/p;
+    if (n%p) {
+        c++;
+    }
     data = (int*)malloc(p*c * sizeof(int));
     for (i = 0; i < n; i++)
       fscanf(file, "%d", &(data[i]));
@@ -92,7 +90,9 @@ int main(int argc, char ** argv)
   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   c = n/p;
-  if (n%p) c++;
+  if (n%p) {
+      c++;
+  }
 
   chunk = (int*)malloc(c * sizeof(int));
   MPI_Scatter(data, c, MPI_INT, chunk, c, MPI_INT, 0, MPI_COMM_WORLD);
@@ -103,10 +103,12 @@ int main(int argc, char ** argv)
   bubblesort(chunk, s);
 
   for (step = 1; step < p; step = 2*step) {
+    // IF NOT MASTER (COMBINER) NODE
     if (id % (2*step)!=0) {
       MPI_Send(chunk, s, MPI_INT, id-step, 0, MPI_COMM_WORLD);
       break;
     }
+    // IF MASTER (COMBINER) NODE
     if (id+step < p) {
       o = (n >= c * (id+2*step)) ? c * step : n - c * (id+step);
       other = (int*)malloc(o * sizeof(int));
@@ -116,6 +118,7 @@ int main(int argc, char ** argv)
       free(other);
       chunk = data;
       s = s + o;
+      // s is the length of the new combined array
     }
   }
 
@@ -123,7 +126,7 @@ int main(int argc, char ** argv)
 
   if (id == 0) {
     file = fopen(argv[2], "w");
-    fprintf(file, "%d\n", s);   // assert (s == n)
+    fprintf(file, "%d\n", s);
     for (i = 0; i < s; i++)
       fprintf(file, "%d\n", chunk[i]);
     fclose(file);
